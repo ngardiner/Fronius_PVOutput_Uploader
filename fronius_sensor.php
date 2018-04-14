128b6441224cfbb0d58785e43156e417d1fed86c
Nathan Gardiner <ngardiner@gmail.com>
2018-04-14 22:46:31 +1000

    Update fronius_sensor.php
    Dummy sensor for HASS template validation

--- fronius_sensor.php
+++ fronius_sensor.php
@@ -16,4 +16,22 @@ if(!$db) {
    echo "Opened database successfully\n";
 }
 
+// Output sensor details in JSON format
+$vals = array(
+   'date' => 0, 
+   'time' => 0, 
+   'inverter_energy_day_total' => 0, 
+   'inverter_power_live' => 0, 
+   'inverter_voltage_live' => 0,
+   'consumption_energy_day_total' => 0,
+   'consumption_power_live' => 0,
+   'meter_export_day_total' => 0,
+   'meter_import_day_total' => 0,
+   'meter_power_live' => 0,
+   'meter_power_live_export' => 0,
+   'meter_power_live_import' => 0
+);
+
+echo json_encode($vals);
+
 ?>
