e94161536d6713acbb4f5e5f583269efd9f25f22
Nathan Gardiner <ngardiner@gmail.com>
2018-04-14 23:08:52 +1000

    Update fronius_sensor.php
    Implemented live data retrieval for HASS sensor

--- fronius_sensor.php
+++ fronius_sensor.php
@@ -13,23 +13,27 @@ $db = new PVOutputDB($sqlFile);
 if(!$db) {
    echo $db->lastErrorMsg();
 } else {
-   echo "Opened database successfully\n";
+   #echo "Opened database successfully\n";
 }
+
+// Query latest previous statistics
+$results = $db->query('SELECT * FROM pvoutput ORDER BY date desc, time desc LIMIT 1');
+$row = $results->fetchArray();
 
 // Output sensor details in JSON format
 $vals = array(
-   'date' => 0, 
-   'time' => 0, 
-   'inverter_energy_day_total' => 0, 
-   'inverter_power_live' => 0, 
-   'inverter_voltage_live' => 0,
-   'consumption_energy_day_total' => 0,
-   'consumption_power_live' => 0,
-   'meter_export_day_total' => 0,
-   'meter_import_day_total' => 0,
-   'meter_power_live' => 0,
-   'meter_power_live_export' => 0,
-   'meter_power_live_import' => 0
+   'date' => $row['date'],
+   'time' => $row['time'],
+   'inverter_energy_day_total' => $row['iEnergyDayTotal'],
+   'inverter_power_live' => $row['iPowerLive'],
+   'inverter_voltage_live' => $row['iVoltageLive'],
+   'consumption_energy_day_total' => $row['cEnergyDayTotal'],
+   'consumption_power_live' => $row['cPowerLive'],
+   'meter_export_day_total' => $row['mExportDayTotal'],
+   'meter_import_day_total' => $row['mImportDayTotal'],
+   'meter_power_live' => $row['mPowerLive'],
+   'meter_power_live_export' => $row['mPowerLiveExport'],
+   'meter_power_live_import' => $row['mPowerLiveImport']
 );
 
 echo json_encode($vals);
